8eef1a8b99d6bbfb06b367c965c6e49d660c80d5
sinisiro <sinisiro@naver.com>
2022-01-05 14:12:57 +0900

    Initial Commit

---
+++ MVPDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPDemo
+//
+//  Created by sinisiro on 2022/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
